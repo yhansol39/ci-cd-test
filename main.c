1a6fae8b0f51571b2e68dfcc521319dc917e63ce
DoraDora <yhansol39@gmail.com>
2025-08-28 22:03:28 +0900

    Build with Artifacts

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 
 
 #include <stdio.h>
+#include <assert.h>
+
+//테스트 할 함수
+int add(int a, int b)
+{
+    return a+b;
+}
 
 int main() {
-    printf("Hello CI/CD World!! \n");
+    printf("=== Program Started ===\n");
+
+    //단위 테스트
+    assert(add(2,3) == 5);
+    assert(add(-1, 1) == 0);
+    assert(add(10, 5) == 15);
+
+    printf("All tests passed!\n");
 
     return 0;
 }
